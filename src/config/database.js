fbf488c3ed0938058fabdde514218108f715b40e
FelipeB4C <felipeb4c@gmail.com>
2020-06-04 22:04:23 -0300

    banco de dados oficial

--- src/config/database.js
+++ src/config/database.js
@@ -1,21 +1,9 @@
-/*module.exports = {
+module.exports = {
   dialect: 'mysql',
   host: 'dicgloss.com',
   username: 'dicglo36_uglossd',
   password: 'gloss@90',
   database: 'dicglo36_glossdb',
-  define: {
-    timestamps: true,
-    underscored: true,
-  },
-};*/
-
-module.exports = {
-  dialect: 'mysql',
-  host: 'dicgloss.com',
-  username: 'dicglo36_uteste',
-  password: 'teste@90',
-  database: 'dicglo36_testegloss',
   define: {
     timestamps: true,
     underscored: true,
